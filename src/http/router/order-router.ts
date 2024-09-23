c2bc5f60591aebe5a200181959b2d09bfa998628
eduardo nakai <eduardofrnkdev@gmail.com>
2024-09-23 15:00:32 -0300

    alterei a order para fazer uma nova versao de seu controle futuro

--- src/http/router/order-router.ts
+++ src/http/router/order-router.ts
@@ -1,16 +1,9 @@
 import { FastifyInstance } from "fastify";
-import { ListOrdersByCustomerIdController } from "../controller/list-orders-by-customer-id.controller";
+
 import { GetByIdOrderController } from "../controller/get-by-id-order.controller";
-import { ListOrdersByProductIdController } from "../controller/list-orders-by-product-id.controller";
-import { RegisterOrderController } from "../controller/register-order.controller";
-import { UpdateOrderController } from "../controller/update-order.controller";
+
+
 
 export async function OrderRouter(app: FastifyInstance) {
     app.get("/:id", GetByIdOrderController)
-    app.get("/listbycustomer", ListOrdersByCustomerIdController)
-    app.get("/listbyproduct", ListOrdersByProductIdController)
-
-    app.post("/", RegisterOrderController)
-    
-    app.put("/",UpdateOrderController)
 }
