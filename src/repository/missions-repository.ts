75760eceeca76890f5e5ca27140c99b626cd75a7
My Little Pet <mylittlepetinternal@gmail.com>
2024-10-29 11:18:25 -0300

    feat: add missions repository

---
+++ src/repository/missions-repository.ts
@@ -0,0 +1,17 @@
+import { Missao, Prisma } from "@prisma/client";
+
+export interface MissionsRepository {
+    findById(id: string): Promise<Missao | null>
+
+    register(data: Prisma.MissaoUncheckedCreateInput): Promise<Missao | null>
+
+    updateComplete(id: string): Promise<Missao | null>
+
+    update(id: string, data: Prisma.MissaoUncheckedUpdateInput): Promise<Missao | null>
+
+    listAllByCustomerIdUncomplete(customerId: string): Promise<Missao[]>
+
+    listAllByCustomerId(customerId: string): Promise<Missao[]>
+
+
+}
