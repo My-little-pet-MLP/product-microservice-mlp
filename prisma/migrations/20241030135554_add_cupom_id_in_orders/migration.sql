56a144ccec218b0b101baa1b8149e5c9da033250
My Little Pet <mylittlepetinternal@gmail.com>
2024-10-30 11:03:26 -0300

    add: add cupomId in orders

---
+++ prisma/migrations/20241030135554_add_cupom_id_in_orders/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Cupom" ALTER COLUMN "ValidateAt" SET DEFAULT '2050-02-09T00:00:00.000Z'::timestamp;
+
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "cupomId" TEXT;
---
+++ prisma/migrations/20241030135744_add_cupomid_in_order/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Cupom" ALTER COLUMN "ValidateAt" SET DEFAULT '2050-02-09T00:00:00.000Z'::timestamp;
